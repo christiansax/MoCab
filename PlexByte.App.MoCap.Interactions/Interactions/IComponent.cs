cd6057ba5779f6ebacb43db36da938a8c5d485d9
christian.sax <christian@sax-news.com>
2015-12-01 22:41:51 +0100

    - Updated Document - Updated Interactions Component

--- PlexByte.App.MoCap.Interactions/Interactions/IComponent.cs
+++ PlexByte.App.MoCap.Interactions/Interactions/IComponent.cs
@@ -3,10 +3,27 @@ namespace Interactions
 {
     public interface IComponent
     {
+        /// <summary>
+        /// The dateTime this item was created
+        /// </summary>
         DateTime CreatedDateTime { get; }
+        /// <summary>
+        /// The dateTime this item was modified
+        /// </summary>
         DateTime ModifiedDateTime { get; }
+        /// <summary>
+        /// The dateTime this item was sent
+        /// </summary>
         DateTime SentDateTime { get; }
+        /// <summary>
+        /// The dateTime this item was read
+        /// </summary>
         DateTime ReadDateTime { get; }
         DateTime DueDateTime { get; }
+        string Id{ get; }
+        string Title { get; }
+        string Description { get; }
+        int Priority { get; }
+        int TimeToLive{ get; }
     }
 }

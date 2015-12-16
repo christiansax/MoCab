4122b304f643ea88f6f929a432a4f18a330a026c
christian.sax <christian@sax-news.com>
2015-12-17 00:56:07 +0100

    - Added template for class - Added template for interface

---
+++ PlexByte.App.MoCap.CodeTemplate/CodeTemplate/ClassTemplate.cs
@@ -0,0 +1,102 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+//                              <ClassTemplate>                             //
+//      Author:     <AuthorName>                Date:       <DateCreated>   //
+//      Project:    <ProjectName>               Reviewed:   <Name>          //
+//      Component:  <ComponentName>             Owner:      <Name>          //
+//      Description: Here goes a short description of the assembly,         //
+//                   explainig what it does, what it is used for and        //
+//                   why it is used                                         //
+//////////////////////////////////////////////////////////////////////////////
+
+#region usings
+#region Includes (Microsoft based)
+//////////////////////////////////////////////
+//      using includes here (Microsoft)     //
+//////////////////////////////////////////////
+#endregion
+
+#region Includes (3rd parties)
+//////////////////////////////////////////////
+//      using includes here (3rd parties)   //
+//////////////////////////////////////////////
+#endregion
+
+#region Includes (MoCap based)
+//////////////////////////////////////////////
+//      using includes here (MoCap)         //
+//////////////////////////////////////////////
+#endregion
+#endregion
+
+namespace MoCap.CodeTemplate
+{
+    #region Delegates are declared here
+
+    #endregion
+
+    #region Global variables are declared here
+
+    #endregion
+
+    #region Enum(-s) or type(-s) declaration here
+
+    #endregion
+
+    /// <summary>
+    /// This is the summary of the class
+    /// </summary>
+    public class ClassTemplate
+    {
+        #region Class members
+
+        #region Events
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Private variables
+
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        #region Constructor(-s) & Destructor
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Event handlers (methods)
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Description of the method, what is it doing
+        /// </summary>
+        /// <param name="pMystring">String: Specifying the name of the object</param>
+        /// <param name="pMyCount">Int: The number of objects to compare</param>
+        /// <returns>object: The object which resulted from the query</returns>
+        public object GetData(string pMystring, int pMyCount)
+        {
+            object a = new object();
+            return (a);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Nested classes here (indentical structure here)
+
+        #endregion
+    }
+}

dcdb26f9422f0b572865fc92dbd68c249c39ee46
christian.sax <christian@sax-news.com>
2016-01-22 01:35:02 +0100

    - Added cypher class to en / decrypt object / streams

---
+++ PlexByte.App.MoCap.Backend/DataService/MoCapWS.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DataService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MoCapWS" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MoCapWS.svc or MoCapWS.svc.cs at the Solution Explorer and start debugging.
+    public class MoCapWS : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}

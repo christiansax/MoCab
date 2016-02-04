e6e89f36aada06c61069715a9665c321e0e2cfa3
Fabian Ochsner <f.ochsn3r@gmail.com>
2016-02-04 19:39:32 +0100

    update and add: properties, constructor

--- PlexByte.App.MoCap.Interactions/Interactions/Project.cs
+++ PlexByte.App.MoCap.Interactions/Interactions/Project.cs
@@ -9,22 +9,56 @@ namespace MoCap.Interactions
     // TODO: <MoCap.Interactions.Project> Start implementing this class
     public class Project : IInteraction
     {
+
+        #region Properties
         public long ID { get; }
         public string Title { get; set; }
         public string Text { get; set; }
         public long CreatorID { get; }
-        public long OwnerID { get; set; }
-        public long ProjectID { get; set; } = -1;
+        public long OwnerID { get; set; }           //probably not needed
+        public long ProjectID { get; set; } = -1;   //<--- what is this for?
         public bool IsCompleted { get; }
         public DateTime Created { get; }
-        public DateTime Modified { get; }
+        public DateTime Modified { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public DateTime Due { get; set; }
 
+
+        public List<long> MemberID { get; set; }
+        public List<long> TaskID { get; set; }
+        #endregion
+
+
+        #region Ctor & Dtor
+        Project(long pID, string pTitle, string pText, long pCreatorID, long pOwnerID, long pProjectID,
+            bool pIsCompleted, DateTime pCreated, DateTime pModified, DateTime pStart, DateTime pEnd, DateTime pDue,
+            List<long> pMemberID, List<long> pTaskID)
+        {
+            ID = pID;
+            Title = pTitle;
+            Text = pText;
+            CreatorID = pCreatorID;
+            OwnerID = pOwnerID;
+            ProjectID = pProjectID;
+            IsCompleted = pIsCompleted;
+            Created = pCreated;
+            Modified = pModified;
+            Start = pStart;
+            End = pEnd;
+            Due = pDue;
+            MemberID = pMemberID;
+            TaskID = pTaskID;
+        }
+
+        #endregion
+
+        #region Methods
         public void Send()
         {
-
+            Modified = DateTime.Now;
         }
+
+        #endregion
     }
 }

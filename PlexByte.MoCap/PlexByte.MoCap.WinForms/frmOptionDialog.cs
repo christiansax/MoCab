be87305926c8f3162967167bb70c64cb685eafcc
christian.sax <christian@sax-news.com>
2016-02-08 00:44:01 +0100

    - Added blank forms for Task, Poll, Option dialog and Vote dialog

---
+++ PlexByte.MoCap/PlexByte.MoCap.WinForms/frmOptionDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlexByte.MoCap.WinForms
+{
+    public partial class frmOptionDialog : Form
+    {
+        public frmOptionDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}

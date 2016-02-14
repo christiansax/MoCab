7f0c1f9220bc8e9320c5c430d3fd9a33bbb3be94
christian.sax <christian@sax-news.com>
2016-02-14 21:01:56 +0100

    - Updated Header

---
+++ PlexByte.App.MoCap.Interactions/PlexByte.MoCap.Interactions/SurveyOption.cs
@@ -0,0 +1,23 @@
+﻿//////////////////////////////////////////////////////////////
+//                      Class SurveyOption                              
+//      Author: Christian B. Sax            Date:   2016/02/13
+using System;
+
+/// <summary>
+/// The survey option class, which hold the option text
+/// </summary>
+public class SurveyOption : ISurveyOption
+{
+	public SurveyOption(string pId, string pText)
+	{
+        CreatedDateTime=DateTime.Now;
+	    Id = pId;
+	    Text = pText;
+	}
+
+    public DateTime CreatedDateTime { get; }
+
+    public string Id { get; }
+
+    public string Text { get; }
+}
---
+++ PlexByte.App.MoCap.Interactions/PlexByte.MoCap.Interactions/Vote.cs
@@ -0,0 +1,45 @@
+﻿//////////////////////////////////////////////////////////////
+//                      Class Vote                              
+//      Author: Christian B. Sax            Date:   2016/02/13
+using System;
+
+/// <summary>
+/// This class holds information about a users choice, containing 
+/// the user and his choice selected
+/// </summary>
+public class Vote : IVote
+{
+    /// <summary>
+    /// The constructor of the class
+    /// </summary>
+    /// <param name="pId">The unique id of this object</param>
+    /// <param name="pUser">The user who made the choice</param>
+    /// <param name="pOption">The choice that was selected</param>
+    public Vote(string pId, IUser pUser, ISurveyOption pOption)
+    {
+        Id = pId;
+        User = pUser;
+        Option = pOption;
+    }
+
+    /// <summary>
+    /// The date and time this vote was left
+    /// </summary>
+    public DateTime CreatedDateTime { get; }
+
+    /// <summary>
+    /// The unique id of this object
+    /// </summary>
+    public string Id { get; }
+
+    /// <summary>
+    /// The survey option chosen by the user
+    /// </summary>
+    public ISurveyOption Option { get; }
+
+    /// <summary>
+    /// The user who votes
+    /// </summary>
+    public IUser User { get; }
+}
+

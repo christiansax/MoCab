73e19f75977af2044437fa4b853de5002af4fbb6
christian.sax <christian@sax-news.com>
2016-03-02 10:00:12 +0100

    - Anchor uc_Survey

--- PlexByte.MoCap/PlexByte.MoCap.WinForms/UserControls/uc_Survey.Designer.cs
+++ PlexByte.MoCap/PlexByte.MoCap.WinForms/UserControls/uc_Survey.Designer.cs
@@ -198,6 +198,8 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.Controls.Add(this.btn_TaskView);
             this.groupBox2.Controls.Add(this.tbx_TaskTitle);
             this.groupBox2.Controls.Add(this.lbl_AssignTask);
@@ -271,6 +273,9 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox3.Controls.Add(this.textBox10);
             this.groupBox3.Controls.Add(this.label9);
             this.groupBox3.Controls.Add(this.btn_Vote);

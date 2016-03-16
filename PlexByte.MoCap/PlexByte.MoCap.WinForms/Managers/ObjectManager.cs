213239a8bb49308283c911a4f907c1930028d560
christian.sax <christian@sax-news.com>
2016-03-16 01:37:25 +0100

    - Added methods to object manager to created objects from the datatable given

--- PlexByte.MoCap/PlexByte.MoCap.WinForms/Managers/ObjectManager.cs
+++ PlexByte.MoCap/PlexByte.MoCap.WinForms/Managers/ObjectManager.cs
@@ -109,43 +109,43 @@ namespace PlexByte.MoCap.WinForms.Managers
 
         public List<T> ProcessResultSet<T>(DataTable pDataTable)
         {
-            List<T> tmp=new List<T>();
+            List<T> tmp = new List<T>();
 
             if (typeof (Project) == typeof (T))
             {
                 // Assemble project interaction from resultset
             }
-            else if(typeof(Task) == typeof(T))
+            else if (typeof (Task) == typeof (T))
             {
                 // Assemble task interaction from resultset
             }
-            else if (typeof(Survey) == typeof(T))
+            else if (typeof (Survey) == typeof (T))
             {
                 // Assemble survey interaction from resultset
             }
-            else if (typeof(Expense) == typeof(T))
+            else if (typeof (Expense) == typeof (T))
             {
                 // Assemble Expense interaction from resultset
             }
-            else if (typeof(Timeslice) == typeof(T))
+            else if (typeof (Timeslice) == typeof (T))
             {
                 // Assemble Timeslice interaction from resultset
             }
-            else if (typeof(SurveyOption) == typeof(T))
+            else if (typeof (SurveyOption) == typeof (T))
             {
                 // Assemble SurveyOption from resultset
             }
-            else if (typeof(Vote) == typeof(T))
+            else if (typeof (Vote) == typeof (T))
             {
                 // Assemble vote from recordset
             }
-            else if (typeof(User) == typeof(T))
+            else if (typeof (User) == typeof (T))
             {
                 // Assemble user from recordset
             }
             else
             {
-                throw new ArgumentException($"The type provided is not implemented. Cannot assemble type {typeof(T).ToString()}");
+                throw new ArgumentException($"The type provided is not implemented. Cannot assemble type {typeof (T).ToString()}");
             }
 
             return tmp;
@@ -172,53 +172,133 @@ namespace PlexByte.MoCap.WinForms.Managers
 
         #region Public methods
 
-        //public List<Project> GetProjectsForUser() 
+        /// <summary>
+        /// This method populates the project list based on the recordset given
+        /// </summary>
+        /// <param name="pResultSet">The recordset containing the projects to create</param>
+        public void CreateProjects(DataTable pResultSet) { ProjectList = CreateObjectFromDataTable<List<Project>>(pResultSet); }
 
-        public T CreateObjectFromDataTable<T>(string pId)
-        {
-            throw new NotImplementedException();
-        }
+        /// <summary>
+        /// This method populates the task list based on the recordset given
+        /// </summary>
+        /// <param name="pResultSet">The recordset containing the tasks to create</param>
+        public void CreateTasks(DataTable pResultSet) { TaskList = CreateObjectFromDataTable<List<Task>>(pResultSet); }
 
-        public T CreateObjectFromDataTable<T>(DataTable pRecordSet)
+        /// <summary>
+        /// This method populates the survey list based on the recordset given
+        /// </summary>
+        /// <param name="pResultSet">The recordset containing the surveys to create</param>
+        public void CreateSurveys(DataTable pResultSet) { SurveyList = CreateObjectFromDataTable<List<Survey>>(pResultSet); }
+
+        /// <summary>
+        /// This method populates the account list based on the recordset given
+        /// </summary>
+        /// <param name="pResultSet">The recordset containing the accounts to create</param>
+        public void CreateAccounts(DataTable pResultSet) { }
+
+        /// <summary>
+        /// This method populates the Expense list based on the recordset given
+        /// </summary>
+        /// <param name="pResultSet">The recordset containing the expenses to create</param>
+        public void CreateExpenses(DataTable pResultSet) { ExpenseList = CreateObjectFromDataTable<List<Expense>>(pResultSet); }
+
+        /// <summary>
+        /// This method populates the Timeslice list based on the recordset given
+        /// </summary>
+        /// <param name="pResultSet">The recordset containing the timeslices to create</param>
+        public void CreateTimeslices(DataTable pResultSet) { TimeSliceList = CreateObjectFromDataTable<List<Timeslice>>(pResultSet); }
+
+        /// <summary>
+        /// This method populates the user list based on the recordset given
+        /// </summary>
+        /// <param name="pResultSet">The recordset containing the users to create</param>
+        public User CreateUsers(DataTable pResultSet) { return CreateObjectFromDataTable<User>(pResultSet); }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// This method creates the object specidied based on the id given. It will lookup the db for the id and context specified and 
+        /// creates the object(-s) in question
+        /// </summary>
+        /// <typeparam name="T">The type of object to create</typeparam>
+        /// <param name="pId">The id of the object to lookup</param>
+        /// <returns>Object of the type specified</returns>
+        private T CreateObjectFromId<T>(string pId)
         {
             T tmp = default(T);
-            if (typeof(IProject) == typeof(T))
+            if (typeof (IProject) == typeof (T))
             {
             }
-            else if (typeof(ITask) == typeof(T))
+            else if (typeof (ITask) == typeof (T))
             {
             }
-            else if (typeof(ISurvey) == typeof(T))
+            else if (typeof (ISurvey) == typeof (T))
             {
             }
-            else if (typeof(IExpense) == typeof(T))
+            else if (typeof (IExpense) == typeof (T))
             {
             }
-            else if (typeof(ITimeslice) == typeof(T))
+            else if (typeof (ITimeslice) == typeof (T))
             {
             }
-            else if (typeof(ISurveyOption) == typeof(T))
+            else if (typeof (ISurveyOption) == typeof (T))
             {
             }
-            else if (typeof(IVote) == typeof(T))
+            else if (typeof (IVote) == typeof (T))
+            {
+            }
+            else if (typeof (ITask) == typeof (T))
             {
             }
             else
-                throw new ArgumentException($"The generic method does not implement functionality for type {typeof(T).ToString()}");
+                throw new ArgumentException($"The generic method does not implement functionality for type {typeof (T).ToString()}");
 
             return tmp;
         }
 
+        /// <summary>
+        /// This method creates the type of object specified based on a fixed column mapping of the data table provided
+        /// </summary>
+        /// <typeparam name="T">The type of object to create</typeparam>
+        /// <param name="pRecordSet">The dataTable which contains the recordset to process</param>
+        /// <returns>Object of type specified</returns>
+        private T CreateObjectFromDataTable<T>(DataTable pRecordSet)
+        {
+            T tmp = default(T);
+            if (typeof (IProject) == typeof (T))
+            {
+            }
+            else if (typeof (ITask) == typeof (T))
+            {
+            }
+            else if (typeof (ISurvey) == typeof (T))
+            {
+            }
+            else if (typeof (IExpense) == typeof (T))
+            {
+            }
+            else if (typeof (ITimeslice) == typeof (T))
+            {
+            }
+            else if (typeof (ISurveyOption) == typeof (T))
+            {
+            }
+            else if (typeof (IVote) == typeof (T))
+            {
+            }
+            else if (typeof (ITask) == typeof (T))
+            {
+            }
+            else
+                throw new ArgumentException($"The generic method does not implement functionality for type {typeof (T).ToString()}");
+
+            return tmp;
+        }
+
+        private void UpdateTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e) { throw new NotImplementedException(); }
+
         #endregion
-
-        private void UpdateTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void CreatedInventory(bool pCheckChanged)
-        {
-
-        }
     }
 }

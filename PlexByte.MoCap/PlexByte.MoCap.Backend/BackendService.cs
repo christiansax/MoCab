fde74366b205bd48846300adaa402549f7c8b833
christian.sax <christian@sax-news.com>
2016-03-24 22:13:30 +0100

    - Fixed stackoverflow exception

--- PlexByte.MoCap/PlexByte.MoCap.Backend/BackendService.cs
+++ PlexByte.MoCap/PlexByte.MoCap.Backend/BackendService.cs
@@ -23,11 +23,16 @@ namespace PlexByte.MoCap.Backend
 
     public class BackendService
     {
-        private static string _DBServer = "198.38.83.33";
-        private static string _Password = "MoCap";
-        private static string _DBUser = CryptoHelper.Decrypt("BjjfgaK9bvsBcQCzh2cA7D0OQnwJ1lrU/36zDXs5bKfbK/mCnQChL74+/wtOu1+6", _Password);
-        private static string _DBPWD = CryptoHelper.Decrypt("mFntrazoOtZJqDO+T8vCdtnm6aUjkfvf0Lh8kATxrZQkfNE0NiPDC7zXkb2h22MM", _Password);
-        private string _connectionString = string.Format("Server={0}; Database=csax2277_MoCap;User Id={1};Password={2};", _DBServer, _DBUser, _DBPWD);
+        private const string _DBServer = "198.38.83.33";
+        private const string _Password = "MoCap";
+        private string _DBUser = CryptoHelper.Decrypt("BjjfgaK9bvsBcQCzh2cA7D0OQnwJ1lrU/36zDXs5bKfbK/mCnQChL74+/wtOu1+6", _Password);
+        private string _DBPWD = CryptoHelper.Decrypt("mFntrazoOtZJqDO+T8vCdtnm6aUjkfvf0Lh8kATxrZQkfNE0NiPDC7zXkb2h22MM", _Password);
+        private string _connectionString = null;
+
+        public BackendService()
+        {
+            _connectionString = string.Format("Server={0}; Database=csax2277_MoCap;User Id={1};Password={2};", _DBServer, _DBUser, _DBPWD);
+        }
 
         /// <summary>
         /// This method authenticates the given user and password against the database. If suceeded the ID of the authenticated user will be returnes

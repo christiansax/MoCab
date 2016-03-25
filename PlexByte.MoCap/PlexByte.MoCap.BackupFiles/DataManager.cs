ffc663f59dd8c04be37e1fc82c077e0b95ab9991
christian.sax <christian@sax-news.com>
2016-03-25 15:11:55 +0100

    - Recommit

---
+++ PlexByte.MoCap/PlexByte.MoCap.BackupFiles/DataManager.cs
@@ -0,0 +1,114 @@
+﻿//////////////////////////////////////////////////////////////
+//                      Class DataManager
+//      Author: Christian B. Sax            Date:   2016/03/14
+//      This class manages is used to convert from Object to SQL parsable data and vice versa.
+//      The objective is to encapsulate any backend related conversions within this class and 
+//      expose objects to calling classes only
+using PlexByte.MoCap.Backend;
+using PlexByte.MoCap.Interactions;
+using PlexByte.MoCap.Managers.OLD;
+using PlexByte.MoCap.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlexByte.MoCap.OLD
+{
+    public class DataManager:IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IProject GetProjectById(string pId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ITask GetTaskById(string pId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ISurvey GetSurveyById(string pId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IExpense GetExpenseById(string pId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ITimeslice GetTimesliceById(string pId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IUser GetUser(string pUserId, bool pIsUserName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpsertExpense(IExpense pExpense)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpsertProject(IProject pProject)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpsertSurvey(ISurvey pSurvey)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpsertTask(ITask pTask)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpsertTimeslice(ITimeslice pTimeslice)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpsertUser(IUser pUser)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ISurveyOption GetSurveyOption(string pId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<SurveyOption> GetSurveyOptions(string pSurveyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpsertSurveyOption(ISurveyOption pSurveyOption)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IVote GetVoteById(string pId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<IVote> GetVoteBySurveyId(string pId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpsertVote(IVote pVote)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

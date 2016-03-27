c19ce93674de448e7cc19fc02d8aeffad54335d2
christian.sax <christian@sax-news.com>
2016-03-27 22:09:16 +0200

    - Fixed is active issue

--- PlexByte.App.MoCap.Backend/PlexByte_MoCap/View_Task.sql
+++ PlexByte.App.MoCap.Backend/PlexByte_MoCap/View_Task.sql
@@ -3,7 +3,8 @@
 --	Date:	2016/03/06
 CREATE VIEW [dbo].[View_Task]
 	AS 
-	SELECT	[t].[Id], [t].[Title], [i].[Text] AS TaskDescription, [t].[InteractionId], [i].[StartDateTime], [i].[EndDateTime], [t].[DueDateTime], [i].[StateId], [s].[Text] AS StateName, [i].[IsActive], [t].[Priority], 
+	SELECT	[t].[Id], [t].[Title], [i].[Text] AS TaskDescription, [t].[InteractionId], [i].[StartDateTime], [i].[EndDateTime], 
+			[t].[DueDateTime], [i].[StateId], [s].[Text] AS StateName, [i].[IsActive], [t].[Priority], 
 			[t].[Progress], [t].[Duration], [t].[Budget], [t].[DurationUsed], [t].[BudgetUsed], [i].[CreatorId], [c].[Username] AS Creator,
 			[i].[OwnerId], [u].[Username] AS [Owner], [t].[ModifiedDateTime], [t].[CreatedDateTime], [i].[Type]
 	FROM	[Task] t INNER JOIN [Interaction] i

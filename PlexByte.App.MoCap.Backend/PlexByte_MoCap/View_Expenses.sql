894932d16d8f7a403f04988ce2a7da3bf85efad8
christian.sax <christian@sax-news.com>
2016-03-28 14:51:21 +0200

    - Added missing expense view - Fixed timesliceview

---
+++ PlexByte.App.MoCap.Backend/PlexByte_MoCap/View_Expenses.sql
@@ -0,0 +1,11 @@
+﻿--	View_Expenses returns a list of expenses with their corresponding projectId
+--	Author:	Christian B. Sax
+--	Date:	2016/03/28
+CREATE VIEW [dbo].[View_Expenses]
+	AS
+	SELECT	[e].*, [a].ProjectId, [a].[TaskId], [u].[Username]
+	FROM	[Expense] e INNER JOIN [Accounting] a
+			ON [e].[Id] = [a].[ExpenseId]
+			INNER JOIN [User] u
+			ON [e].[CreatorId] = [u].[Id]
+	WHERE	[a].[ExpenseId] IS NOT NULL
--- PlexByte.App.MoCap.Backend/PlexByte_MoCap/View_Timeslice.sql
+++ PlexByte.App.MoCap.Backend/PlexByte_MoCap/View_Timeslice.sql
@@ -2,8 +2,10 @@
 --	Author:	Christian B. Sax
 --	Date:	2016/03/06
 CREATE VIEW [dbo].[View_Timeslice]
-	AS 
-	SELECT	[t].[Id], [t].[Duration], [t].[Description], [t].[CreatorId], [u].[Username], [t].[ModifiedDateTime], 
-			[t].[CreatedDateTime]
-	FROM	[Timeslice] t INNER JOIN [View_User] u
-	ON		[u].Id = [t].[CreatorId]
+	AS
+	SELECT	[e].*, [a].ProjectId, [a].[TaskId], [u].[Username]
+	FROM	[Timeslice] e INNER JOIN [Accounting] a
+			ON [e].[Id] = [a].[TimesliceId]
+			INNER JOIN [User] u
+			ON [e].[CreatorId] = [u].[Id]
+	WHERE	[a].[TimesliceId] IS NOT NULL
